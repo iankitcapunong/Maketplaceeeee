5ddaf271702f225f6f55a8a3dbb5f6e414a0217c
Ian Kit Capunong <yayan.cap12@gmail.com>
2025-05-07 04:58:06 +0800

    feat: MainDashboard with functionalities

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
-import DashboardView from '@/views/Product/DashboardView.vue'
+import ProductView from '@/views/Product/ProductView.vue'
 import OrderView from '@/views/Order/OrderView.vue'
-import OrderHistory from '../views/Order/OrderHistory.vue'
-import AccountView from '../views/Account/AccountView.vue'
+import OrderHistory from '@/views/Order/OrderHistory.vue'
+import AccountView from '@/views/Account/AccountView.vue'
+import MainDashboard from '@/views/mainDashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,7 +23,7 @@ const router = createRouter({
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: DashboardView,
+      component: ProductView,
     },
     {
       path: '/orderView',
@@ -38,6 +39,11 @@ const router = createRouter({
       path: '/accountview',
       name: 'accountview',
       component: AccountView,
+    },
+    {
+      path: '/maindashboard',
+      name: 'maindashboard',
+      component: MainDashboard,
     }
   ],
 })
