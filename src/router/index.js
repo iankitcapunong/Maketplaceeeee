c26a3b7b7510939e2b348013656baa1085d2c54f
kathleendemain <kathleendomenique.demain@carsu.edu.ph>
2025-05-07 01:48:41 +0800

    fix: merging

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 
 import RegisterView from '@/views/auth/RegisterView.vue'
-import DashboardView from '@/views/Product/dashboardView.vue'
+import DashboardView from '@/views/Product/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,7 +17,11 @@ const router = createRouter({
       name: 'register',
       component: RegisterView,
     },
-   
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView,
+    },
   ],
 })
 
