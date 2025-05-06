ac3e59bf7ad0bfcefe3c8f3ea815ee58da4f6d98
Ian Kit Capunong <yayan.cap12@gmail.com>
2025-05-07 04:16:07 +0800

    Feat: AccontView with functionalities

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import RegisterView from '@/views/auth/RegisterView.vue'
 import DashboardView from '@/views/Product/DashboardView.vue'
 import OrderView from '@/views/Order/OrderView.vue'
 import OrderHistory from '../views/Order/OrderHistory.vue'
+import AccountView from '../views/Account/AccountView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -33,6 +34,11 @@ const router = createRouter({
       name: 'orderhistory',
       component: OrderHistory,
     },
+    {
+      path: '/accountview',
+      name: 'accountview',
+      component: AccountView,
+    }
   ],
 })
 
